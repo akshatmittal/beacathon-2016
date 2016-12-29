c54aadd42c665083028e20a3de67da0a3a8cc0e2
Akshat Mittal <itsreallyakshat@gmail.com>
2016-12-29 15:12:05 +0530

    CM

---
+++ backend/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$mysqli = new mysqli('localhost','root','root','cn');
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .')');
+}
+
+if(empty($_POST)) {
+  die("Invalid");
+}
+if(!isset($_POST['version'])) {
+  die("No version");
+}
+
+if(isset($_POST['action']) && $_POST['action'] == "fetch") {
+  if(!isset($_POST['uuid'])) {
+    die("No UUID");
+  }
+  $uuid = ($_POST['uuid']);
+
+  $sql = "SELECT * FROM users WHERE uuid = '$uuid'";
+  $results = $mysqli->query($sql);
+  $row = $results->fetch_assoc();
+  die(json_encode($row));
+}
+
+if(isset($_POST['action']) && $_POST['action'] == "order") {
+  if(!isset($_POST['uuid'])) {
+    die("No UUID");
+  }
+  $uuid = ($_POST['uuid']);
+
+  $sql = "SELECT * FROM users WHERE uuid = '$uuid'";
+  $results = $mysqli->query($sql);
+  $row = $results->fetch_assoc();
+  die(json_encode($row));
+}
